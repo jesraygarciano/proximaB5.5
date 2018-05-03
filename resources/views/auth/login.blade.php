fddb84d28303b0efb45a82b6cfb30ad834aa4980
Jesray Garciano <jesray.garciano@gmail.com>
2018-05-03 15:54:07 +0800

    Navigation Layouts directory updates

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,6 +1,16 @@
 @extends('layouts.main-layout')
 
 @section('content')
+    @if (session('status'))
+        <div class="alert alert-success">
+            {{ session('status') }}
+        </div>
+    @endif
+    @if (session('warning'))
+        <div class="alert alert-warning">
+            {{ session('warning') }}
+        </div>
+    @endif
 <div class="container">
     <div class="row">
         <div class="col-md-8 col-md-offset-2">
--- resources/views/layouts/navbar.blade.php
+++ resources/views/layouts/navbar.blade.php
@@ -1,3 +1,15 @@
+<style>
+    .notification-list{
+        list-style:none;
+        padding:0px;
+    }
+    .notification-list li{
+        margin-bottom:5px;
+    }
+    .notification-list li .fa-check-square-o{
+        color:#4cae4c;
+    }
+</style>
 <nav class="navbar navbar-default navbar-static-top">
     <div class="container">
         <div class="navbar-header">
@@ -16,7 +28,7 @@
             </a>
         </div>
 
-        @if(\Auth::check() && \Auth::user()->profileProgress() < 100)
+        @if(\Auth::check())
             <ul class="nav navbar-nav navbar-right" style="margin: initial;">
                 <li class="notification_li" id="notification_li">
                     <a href="#" id="notificationLink">
@@ -30,16 +42,34 @@
                             <div id="notificationsBody" class="notifications">
                                 <div class="noti-content">
                                     {{--  <h4>Updates</h4>  --}}
-                                    <h5>Profile progress:</h5>
-                                    <div class="progress progress-navbar">
-                                        <div class="progress-bar progress-bar-striped active profile-progress" role="progressbar" style="width: {{\Auth::user()->profileProgress()}}%" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100"><span class="val">{{\Auth::user()->profileProgress()}}</span>% profile complete</div>
+                                    <div>
+                                        <ul class="notification-list">
+                                            <li style="padding: 10px;background: ghostwhite;">
+                                                <h4 style="border-bottom: 1px solid #cecece;padding-bottom: 10px;">
+                                                    <i class="fa fa-check-square-o" aria-hidden="true"></i> Application Approved
+                                                    <div>
+                                                    <small class="form-text text-muted">May 4, 2018 8:30am</small>
+                                                    </div>
+                                                </h4>
+                                                After considering your skills, we are happy to let you know that you are qualified to
+                                                undergo our training program.
+                                                <hr>
+                                                <label>Batch : </label> Batch 1
+                                                <br>
+                                                <label> Schedule : </label> July 18, 2018
+                                            </li>
+                                        </ul>
                                     </div>
+                                    <div id="notificationFooter"><a href="#">See All</a></div>
+                                    @if(\Auth::user()->profileProgress() < 100)
+                                        <div class="progress progress-navbar">
+                                            <div class="progress-bar progress-bar-striped active profile-progress" role="progressbar" style="width: {{\Auth::user()->profileProgress()}}%" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100"><span class="val">{{\Auth::user()->profileProgress()}}</span>% profile complete</div>
+                                        </div>
 
-                                    <div class="text-primary"><center>The more information you provide for us, the higher is your chance to be qualified.</center></div>
+                                        <div class="text-primary"><center>The more information you provide for us, the higher is your chance to be qualified.</center></div>
+                                    @endif
                                 </div>
                             </div>
-
-                            <div id="notificationFooter"><a href="{{ route('itp_applicant_profile') }}">See All</a></div>
                     </div>
                 </li>
             </ul>
@@ -111,9 +141,50 @@
             </ul>
         </div>
     </div>
-    </nav>
-
-    <script>
+</nav>
+<div class="ui modal" id="notifications_modal">
+    <div class="header">
+        Notifications
+    </div>
+    <div class="content">
+        <div class="list" style="height:250px; overflow:auto;">
+            <ul class="notification-list">
+                <li style="padding: 10px;background: ghostwhite;">
+                    <h4 style="border-bottom: 1px solid #cecece;padding-bottom: 10px;">
+                        <i class="fa fa-check-square-o" aria-hidden="true"></i> Application Approved
+                        <div>
+                        <small class="form-text text-muted">May 4, 2018 8:30am</small>
+                        </div>
+                    </h4>
+                    After considering your skills, we are happy to let you know that you are qualified to
+                    undergo our training program.
+                    <hr>
+                    <label>Batch : </label> Batch 1
+                    <br>
+                    <label> Schedule : </label> July 18, 2018
+                </li>
+                <li style="padding: 10px;background: ghostwhite;">
+                    <h4 style="border-bottom: 1px solid #cecece;padding-bottom: 10px;">
+                        <i class="fa fa-check-square-o" aria-hidden="true"></i> Application Approved
+                        <div>
+                        <small class="form-text text-muted">May 4, 2018 8:30am</small>
+                        </div>
+                    </h4>
+                    After considering your skills, we are happy to let you know that you are qualified to
+                    undergo our training program.
+                    <hr>
+                    <label>Batch : </label> Batch 1
+                    <br>
+                    <label> Schedule : </label> July 18, 2018
+                </li>
+            </ul>
+        </div>
+    </div>
+    <div class="actions">
+      <div class="ui black button deny">Close</div>
+    </div>
+</div>
+<script>
     $(document).ready(function(){
         $(document).ajaxSuccess(function(ev,req) {
             if(req.responseJSON.profile_progess){
@@ -128,6 +199,30 @@
     });
 
     $(document).ready(function(){
+        $('#notificationContainer').unickNotifier({
+            fetch_url:"{{route('fetch_latest_notifications')}}"
+        }, function(data){
+            // 
+            $('#notificationContainer .notification-list').append('<li style="padding: 10px;background: ghostwhite;">'
+                +'    <h4 style="border-bottom: 1px solid #cecece;padding-bottom: 10px;">'
+                +'        <i class="fa fa-check-square-o" aria-hidden="true"></i> Application Approved'
+                +'        <div>'
+                +'        <small class="form-text text-muted">'+data.created_at+'</small>'
+                +'        </div>'
+                +'    </h4>'
+                +data.explanation
+                +'    <hr>'
+                +'    <label>Batch : </label>'
+                +data.internshipApplication.training_batch.name
+                +'    <br>'
+                +'    <label> Start Date : </label> '
+                +data.internshipApplication.training_batch.start_date
+                +'<br><label>Time Schedule : </label> '+data.internshipApplication.training_batch.schedule
+                +'</li>'
+            );
+        }, null, function(){
+            $('#notificationContainer .notification-list').html('');
+        });
         // $("#notificationLink").click(function(){
             @if(\Auth::check() && \Auth::user()->profileProgress() < 26)
                 $("#notificationLink").css({"color" : "red"});
@@ -158,6 +253,65 @@
         // return false;
     });
 
+    $(document).ready(function(){
+        var latest_id = 0;
+
+        $('#notificationFooter').click(function(){
+            loadAllNotification(latest_id);
+            $('#notifications_modal').modal('show');
+        });
+
+        var loadAllNotification = function(_latest_id){
+            swal({
+                    title: 'Loading Info',
+                    text: 'Please wait...',
+                    onOpen: () => {
+                        swal.showLoading()
+                    },
+                    allowOutsideClick: () => !swal.isLoading()
+            })
+            $.ajax({
+                url:"{{route('fetch_notifications')}}",
+                type:'GET',
+                data:{latest_id:_latest_id},
+                success:function(data){
+                    data = data.notifications;
+                    var i = 0;
+                    swal.close();
+                    for(index in data){
+                        $('#notifications_modal .notification-list').append(
+                            '<li style="padding: 10px;background: ghostwhite;">'
+                            +'    <h4 style="border-bottom: 1px solid #cecece;padding-bottom: 10px;">'
+                            +'        <i class="fa fa-check-square-o" aria-hidden="true"></i> Application Approved'
+                            +'        <div>'
+                            +'        <small class="form-text text-muted">'+data[index].created_at+'</small>'
+                            +'        </div>'
+                            +'    </h4>'
+                            +data[index].explanation
+                            +'    <hr>'
+                            +'    <label>Batch : </label>'
+                            +data[index].internshipApplication.training_batch.name
+                            +'    <br>'
+                            +'    <label> Start Date : </label> '
+                            +data[index].internshipApplication.training_batch.start_date
+                            +'<br><label>Time Schedule : </label> '+data[index].internshipApplication.training_batch.schedule
+                            +'</li>')
+                            latest_id = index == 0 ? data[index].id :latest_id;
+                    }
+                }
+            });
+        }
+
+        $('#notifications_modal .notification-list').html('');
+
+        $('#notifications_modal .list').scroll(function(){
+            if($(this).scrollTop() == ($('#notifications_modal .list')[0].scrollHeight - $('#notifications_modal .list').outerHeight())){
+                console.log('bottom most');
+                loadAllNotification(latest_id);
+            }
+        });
+    });
+
     function setNotificationBackdrop(){
         @if(\Auth::check() && \Auth::user()->profileProgress() < 26)
         var div = document.createElement('div');
@@ -173,6 +327,6 @@
             $(div).remove();
         });
     }
-    setNotificationBackdrop();
-        });
-    </script>
+    // setNotificationBackdrop();
+    });
+</script>
